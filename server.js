06656bc1f27698b2f5cf55f49569d1095a81ac2d
Karen Lo <k8lo@ucsd.edu>
2016-07-13 12:25:02 -0700

    added a cloud database from mongolab

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+// Server code for website
+// MongoLab Cloud Server: https://mlab.com/home
+
 var express = require("express");
 var bodyParser = require("body-parser");
+var MongoClient = require("mongodb").MongoClient;
+var db;
 
 var app = express();
 var router = express.Router();
@@ -7,35 +12,58 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 var path = __dirname + '/app/views/';
 
+// Creating static links
 app.use(express.static('public'));
 app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+
+// Connect to database
+MongoClient.connect('mongodb://admin:visitme@ds053972.mlab.com:53972/visitme', 
+	                (err, database) => {
+	if(err) return console.log(err);
+	db = database;
+
+	// Create local host
+	app.listen(3000,function(){
+	  console.log("Live at Port 3000");
+	});
+});
+
 
 router.use(function(req,res,next) {
   console.log("/" + req.method);
   next();
 });
 
+// Serve index.html
 router.get("/", function(req,res){
   res.sendFile(path + "index.html");
 });
 
 app.use("/", router);
 
-//app.use("*", function(req,res){
-//  res.sendFile(path + "404.html");
-//});
-
-app.post("/process_post", urlencodedParser, function(req, res) {
+// Process form
+app.post("/visitors", urlencodedParser, function(req, res) {
 	response = {
 		firstname: req.body.firstname,
 		lastname: req.body.lastname,
 		age: req.body.age,
 		sex: req.body.optradio
 	};
+
+	// Save to database
+	db.collection('visitors').save(response, (err, result) => {
+	 	if(err) {
+	 		console.log(err);
+	 	} else {
+			console.log("Saved to database");
+		}
+	});
+
 	console.log(response);
 	res.end(JSON.stringify(response));
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+// Serve 404 if page not found
+app.use("*", function(req,res){
+  res.sendFile(path + "404.html");
 });
